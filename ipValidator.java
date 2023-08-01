b80c507ef4f1c84ec5e1e43e12c3f91de71fae2b
TaeHyun <61714078+Taehyun06-Dev@users.noreply.github.com>
2023-08-01 16:55:33 +0900

    Create ipValidator.java

---
+++ ipValidator.java
@@ -0,0 +1,48 @@
+package com.taehyunDev;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ipValidator implements Runnable{
+    private Integer backIP;
+
+    public ipValidator(Integer backIP){
+        this.backIP = backIP;
+    }
+
+    private Boolean checkIpPort(String tempIP, Integer tempPort){
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(tempIP, tempPort), 1500);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private int checkIP(String tempIP){
+        if(checkIpPort(tempIP, 443)){
+            return 2;
+        }
+        if(checkIpPort(tempIP, 22)){
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public void run() {
+        String ipFormat = bmcViewer.IP_FORMAT;
+        int tempResult = checkIP(ipFormat+backIP);
+        bmcViewer.increaseTask();
+        if(tempResult == 2){
+            bmcViewer.addIP(backIP, false);
+            return;
+        }
+        if(tempResult == 1){
+            bmcViewer.addIP(backIP, true);
+            return;
+        };
+    }
+}
