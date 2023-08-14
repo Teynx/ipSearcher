9a21c7f9384686d7d38485ebc00b867e6ec99313
TaeHyun <61714078+Taehyun06-Dev@users.noreply.github.com>
2023-08-14 17:54:13 +0900

    Update ipValidator.java

--- ipValidator.java
+++ ipValidator.java
@@ -4,45 +4,44 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class ipValidator implements Runnable{
+public class ipValidator implements Runnable {
     private Integer backIP;
 
-    public ipValidator(Integer backIP){
+    public ipValidator(Integer backIP) {
         this.backIP = backIP;
     }
 
-    private Boolean checkIpPort(String tempIP, Integer tempPort){
+    private Boolean checkIpPort(String tempIP, Integer tempPort) {
         try {
             Socket socket = new Socket();
             socket.connect(new InetSocketAddress(tempIP, tempPort), 1500);
+            socket.close();
             return true;
         } catch (IOException e) {
             return false;
         }
     }
 
-    private int checkIP(String tempIP){
-        if(checkIpPort(tempIP, 443)){
+    private int checkIP(String tempIP) {
+        if (checkIpPort(tempIP, 443)) {
             return 2;
         }
-        if(checkIpPort(tempIP, 22)){
+        if (checkIpPort(tempIP, 22)) {
             return 1;
         }
         return 0;
     }
 
-    @Override
     public void run() {
-        String ipFormat = bmcViewer.IP_FORMAT;
-        int tempResult = checkIP(ipFormat+backIP);
+        String ipFormat = "192.168.0.";
+        int tempResult = checkIP(ipFormat + backIP);
         bmcViewer.increaseTask();
-        if(tempResult == 2){
-            bmcViewer.addIP(backIP, false);
+        if (tempResult == 2) {
+            bmcViewer.addIP(this.backIP,false);
             return;
         }
-        if(tempResult == 1){
-            bmcViewer.addIP(backIP, true);
-            return;
-        };
+        if (tempResult == 1) {
+            bmcViewer.addIP(this.backIP, true);
+        }
     }
 }
